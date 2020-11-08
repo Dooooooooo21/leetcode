87256da182f53d7a62f58f3358d954b2eaf2e175
dly <1083404373@qq.com>
2020-11-08 23:05:48 +0800

    投票

--- offer/39.py
+++ offer/39.py
@@ -14,3 +14,33 @@ class Solution(object):
         nums = sorted(nums)
 
         return nums[len(nums) // 2]
+
+    def majorityElement2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        tmp = nums[0]
+        votes = 0
+
+        for num in nums:
+            if votes == 0:
+                tmp = num
+            if num == tmp:
+                votes += 1
+            else:
+                votes -= 1
+
+        count = 0
+        for num in nums:
+            if num == tmp:
+                count += 1
+
+        if count >= len(nums) // 2:
+            return tmp
+        else:
+            return 0
+
+
+s = Solution()
+print(s.majorityElement2([1, 2, 3, 2, 2, 2, 5, 4, 2]))
