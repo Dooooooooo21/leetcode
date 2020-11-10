efaaa4719da6fc7fdd9d6aa14db4ae426ccc7493
dly <1083404373@qq.com>
2020-11-10 23:18:41 +0800

    位运算

--- offer/65.py
+++ offer/65.py
@@ -3,7 +3,7 @@
 # @Time    : 2020/9/26 22:57
 # @Author  : dly
 # @File    : 65.py
-# @Desc    :
+# @Desc    : 位运算
 
 class Solution(object):
     def add(self, a, b):
@@ -12,8 +12,12 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        list = [a, b]
-        return sum(list)
+        x = 0xffffffff
+        a, b = a & x, b & x
+        while b != 0:
+            a, b = a ^ b, (a & b) << 1 & x
+
+        return a if a <= 0x7fffffff else ~(a ^ x)
 
 
 print(Solution().add(1, 2))
