4e7dff9a90a373568acfdbc30363e36a7a12daba
dly <1083404373@qq.com>
2020-11-14 00:02:24 +0800

    搜索旋转排序数组

---
+++ offer/33.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/5 23:26
+# @Author  : dly
+# @File    : 33.py
+# @Desc    :
+
+class Solution(object):
+    def verifyPostorder(self, postorder):
+        """
+        :type postorder: List[int]
+        :rtype: bool
+        """
+
