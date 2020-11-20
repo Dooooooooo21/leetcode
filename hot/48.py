22d127d40b29909ab782576cf48f2a56e46599fd
dly <1083404373@qq.com>
2020-11-20 23:01:07 +0800

    旋转图像

---
+++ hot/48.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/20 22:53
+# @Author  : dly
+# @File    : 48.py
+# @Desc    :
+
+
+# 旋转图像
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range(i, n - i - 1):
+                matrix[i][j], matrix[j][n - i - 1], matrix[n - i - 1][n - j - 1], matrix[n - j - 1][i] = \
+                    matrix[n - j - 1][i], matrix[i][j], matrix[j][n - i - 1], matrix[n - i - 1][n - j - 1]
