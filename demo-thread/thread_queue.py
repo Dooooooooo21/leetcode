eb0c44f7a318d530889dcf374c762c465a8f1942
dly <1083404373@qq.com>
2020-11-21 15:25:49 +0800

    换目录

---
+++ demo-thread/thread_queue.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/20 14:56
+# @Author  : dly
+# @File    : thread_queue.py
+# @Desc    :
+
+import time
+import queue
+import threading
+
+
+class Worker(threading.Thread):
+    def __init__(self, name, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        # 执行
+        self.start()
+
+    def run(self):
+        while True:
+            # 队列为空 推出线程
+            if self.queue.empty():
+                break
+            # 获取队列数据
+            foo = self.queue.get()
+            time.sleep(1)
+            print(self.getName() + ' process ' + str(foo))
+            # 任务完成
+            self.queue.task_done()
+
+
+# 队列
+q = queue.Queue()
+# 加入100个任务队列
+for i in range(100):
+    q.put(i)
+
+# 10个线程
+for i in range(10):
+    threadName = 'thread' + str(i)
+    Worker(threadName, q)
+
+q.join()
