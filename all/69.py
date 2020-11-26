03a11f02070d1341e596fe20d39a25cf1dc38268
dly <1083404373@qq.com>
2020-11-26 22:31:45 +0800

    平方根

---
+++ all/69.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/26 22:28
+# @Author  : dly
+# @File    : 69.py
+# @Desc    :
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x ** 0.5)
